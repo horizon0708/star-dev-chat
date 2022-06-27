a62a3a415d9332be9b747c5c7db90c5947d098ef
James Kim <james@sidenotes.xyz>
2022-06-27 17:48:56 +0900

    setup react-router, write sample tests

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,21 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { Navbar } from './components/Navbar/Navbar';
 import { Layout } from './components/Layout';
 import { SignUp } from './pages/SignUp/SignUp';
+import { Routes, Route } from 'react-router-dom';
+import { paths } from './services/path';
+import { Home } from './pages/Home/Home';
+import { Profile } from './pages/Profile/Profile';
 
 function App() {
   return (
     <ChakraProvider>
       <Navbar />
       <Layout>
-        <SignUp />
+        <Routes>
+          <Route path={paths.signUp} element={<SignUp />} />
+          <Route path={paths.profile} element={<Profile />} />
+          <Route path={paths.home} element={<Home />} />
+        </Routes>
       </Layout>
     </ChakraProvider>
   );
