45f3d53f185598c4a6bf241ecf6931aeaf7ed0fa
Behrouz Rozmeh <behrouz.rozmeh@gmail.com>
2022-06-27 13:30:34 +0900

    navbar id updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,13 +6,23 @@ export const Navbar = () => {
   return (
     <Box py={4}>
       <Container maxW="container.xl" >
-        <HStack justifyContent="space-between" h="50px" bg="#f6f6f6f6" >
-          <Heading p={4} color="#0f9d58" size="md" style={{textDecoration: 'none'}} as={Link} href={paths.home}>
+        <HStack justifyContent="space-between" h="50px" bg="blackAlpha.200" >
+          <Heading p={4} color="green.500" size="md" sx={{textDecoration: 'none'}} as={Link} href={paths.home}>
             Star Dev Chat
           </Heading>
-          <HStack p={4} spacing={4} m={4} bg="#069d58" color="#fff" isRound='true' fontSize="large"  >
-            <Link  style={{textDecoration: 'none'}} href={paths.profile}>Profile</Link>
-          </HStack>
+          <HStack  >
+            <Link  sx={{textDecoration: 'none'}}
+                   href={paths.profile} 
+                   p={2}
+                   m={4}
+                   bg="green.500"
+                   color="white"
+                   borderRadius="md"
+                   fontSize="large"
+                   boxShadow="md"
+                   textShadow="sm"
+                               >Profile</Link>
+          </HStack> 
         </HStack>
       </Container>
     </Box>
