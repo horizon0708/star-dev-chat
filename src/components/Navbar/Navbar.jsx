cea54eb74ad8501a123b8d201a21d1bc531ce468
Behrouz Rozmeh <behrouz.rozmeh@gmail.com>
2022-07-07 18:56:27 +0900

    Sign up & Sign in when a user is logged out

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
- import {
+import {
   Box,
   Container,
   Heading,
@@ -8,21 +8,32 @@
 import { Link } from 'react-router-dom';
 import React from 'react';
 import { paths } from '../../services/path';
+import { supabase } from '../../services/supabaseClient';
 
 export const Navbar = () => {
+  const user = supabase.auth.user();
+
   return (
     <Box py={4}>
       <Container maxW="container.xl">
         <HStack justifyContent="space-between">
-          <Heading id='test-home' size="md" as={Link} to={paths.home}>
+          <Heading id="test-home" size="md" as={Link} to={paths.home}>
             Star Dev Chat
           </Heading>
           <HStack spacing={4}>
             <LinkC as={Link} to={paths.profile}>
-              Profile 
+              Profile
             </LinkC>
-            <LinkC as={Link} to={paths.signup}>Sign up</LinkC>
-            <LinkC as={Link} to={paths.signin}>Sign in</LinkC>
+            {!user && (
+              <LinkC as={Link} to={paths.signup}>
+                Sign up
+              </LinkC>
+            )}
+            {!user && (
+              <LinkC as={Link} to={paths.signin}>
+                Sign in
+              </LinkC>
+            )}
           </HStack>
         </HStack>
       </Container>
