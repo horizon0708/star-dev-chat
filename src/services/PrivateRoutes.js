c0bc572103653749afe1e0b55e1b1ac6b16b1be6
Behrouz Rozmeh <behrouz.rozmeh@gmail.com>
2022-07-28 00:35:53 +0900

    routes are updated

--- src/services/PrivateRoutes.js
+++ src/services/PrivateRoutes.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { Navigate, Outlet } from 'react-router';
-import { supabase } from './supabaseClient';
 import { paths } from './path';
+import { AuthProvider } from './Auth';
 
 const PrivateRoutes = () => {
-    let user = supabase.auth.user();
 
-    return (
-     
+    let user = useContext(AuthProvider)
+
+    return (   
           user ? <Outlet/> : <Navigate to = {paths.signin}/> 
-   
     );
+    
 };
 
 export default PrivateRoutes;
--- src/services/RestrictedRoutes.js
+++ src/services/RestrictedRoutes.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { Navigate, Outlet } from 'react-router';
 import { supabase } from './supabaseClient';
 import { paths } from './path';
+import { AuthProvider } from './Auth';
 
 const RestrictedRoutes = () => {
-    let user = supabase.auth.user();
+
+    let user = useContext(AuthProvider);
 
     return (
         !user ? <Outlet/> : <Navigate to = {paths.home}/> 
-
     );
+    
 };
 
 export default RestrictedRoutes;
