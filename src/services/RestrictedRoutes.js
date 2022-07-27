972c8c79d49b58812cc6c349ca4b48868be0f583
Behrouz Rozmeh <behrouz.rozmeh@gmail.com>
2022-07-28 01:15:25 +0900

    small updated

--- src/services/RestrictedRoutes.js
+++ src/services/RestrictedRoutes.js
@@ -1,6 +1,5 @@
 import React, {useContext} from 'react';
 import { Navigate, Outlet } from 'react-router';
-import { supabase } from './supabaseClient';
 import { paths } from './path';
 import { AuthProvider } from './Auth';
 
@@ -11,7 +10,7 @@ const RestrictedRoutes = () => {
     return (
         !user ? <Outlet/> : <Navigate to = {paths.home}/> 
     );
-    
+
 };
 
 export default RestrictedRoutes;
