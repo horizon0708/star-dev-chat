77001fa8609e704784cd3e000a40b1b52a94303f
Behrouz Rozmeh <behrouz.rozmeh@gmail.com>
2022-08-04 19:10:22 +0900

    signout is updated

--- src/pages/SignOut/SignOut.jsx
+++ src/pages/SignOut/SignOut.jsx
@@ -4,12 +4,17 @@ import { useToast } from '@chakra-ui/react';
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router';
 import { paths } from '../../services/path';
+import { useRef } from 'react';
 
 export default function SignOut() {
     const toast = useToast();
     const navigate = useNavigate();
+    const hasRun = useRef(false);
 
    useEffect(()=>{
+    if(hasRun.current){
+        return;
+    }
     supabase.auth.signOut().then(() => {
         navigate(paths.signin);
     })
@@ -18,9 +23,8 @@ export default function SignOut() {
         title: 'Logged out',
         description: 'Sign In again!',
       });
-   }
-   ,[])
-
+      hasRun.current = true;
+   },[hasRun.current]);
 return <> 
       
 </>
